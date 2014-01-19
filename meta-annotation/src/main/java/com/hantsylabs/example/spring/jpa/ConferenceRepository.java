4bced4d7be171d1827a531a2da38b999e80358bb
hantsy <hantsy@gmail.com>
2014-01-19 17:02:31 +0800

    update the pom.xml version and add annotation example.

---
+++ meta-annotation/src/main/java/com/hantsylabs/example/spring/jpa/ConferenceRepository.java
@@ -0,0 +1,15 @@
+package com.hantsylabs.example.spring.jpa;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hantsylabs.example.spring.model.Conference;
+
+@Repository
+public interface ConferenceRepository extends 
+		JpaRepository<Conference, Long> {
+
+	Conference findBySlug(String slug);
+
+}
