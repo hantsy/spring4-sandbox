c9271d257af70787a4ba1845d23a5e25dd990332
hantsy <hantsy@gmail.com>
2015-05-29 16:17:26 +0800

    add WebConfig.java

--- data-rest/src/main/java/com/hantsylabs/example/spring/config/WebConfig.java
+++ data-rest/src/main/java/com/hantsylabs/example/spring/config/WebConfig.java
@@ -1,11 +1,13 @@
 package com.hantsylabs.example.spring.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.webmvc.BaseUri;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig extends RepositoryRestMvcConfiguration{
+
 
 }
