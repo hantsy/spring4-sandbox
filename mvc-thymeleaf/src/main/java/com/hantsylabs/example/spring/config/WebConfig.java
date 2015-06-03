437e5cb57083241d89fb125eb8b745c745fbcce7
hantsy <hantsy@gmail.com>
2015-06-03 22:02:49 +0800

    add thymeleaf configuration.

--- mvc-thymeleaf/src/main/java/com/hantsylabs/example/spring/config/WebConfig.java
+++ mvc-thymeleaf/src/main/java/com/hantsylabs/example/spring/config/WebConfig.java
@@ -1,9 +1,6 @@
 package com.hantsylabs.example.spring.config;
 
-import javax.inject.Inject;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
@@ -15,35 +12,80 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(
-    basePackages= {"com.hantsylabs.example.spring"},
-    useDefaultFilters = false,
-    includeFilters = {
-        @Filter(
-            type = FilterType.ANNOTATION,
-            value = {
-                Controller.class,
-                RestController.class,
-                ControllerAdvice.class
-            })
-    }
-)
+@ComponentScan(basePackages = { "com.hantsylabs.example.spring" }, useDefaultFilters = false, includeFilters = { @Filter(type = FilterType.ANNOTATION, value = {
+		Controller.class, RestController.class, ControllerAdvice.class }) })
 public class WebConfig extends SpringDataWebConfiguration {
+	
+    
 
     @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
+	public void addViewControllers(ViewControllerRegistry registry) {
+    	registry.addRedirectViewController("/", "/tasks");
+		registry
+			.addViewController("/add")
+			.setViewName("add");
+		
+	}
 
-    @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer.favorParameter(false);
-        configurer.favorPathExtension(false);
-    }
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry
+			.viewResolver(viewResolver());
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry
+			.addResourceHandler("webjars/**")
+	        .addResourceLocations("classpath:META-INF/resources/webjars/");
+	}
+	
+	@Bean
+	public ThymeleafViewResolver viewResolver() {
+		ThymeleafViewResolver viewResolver=new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(templateEngine());
+		
+		return viewResolver;
+	}
+
+	@Bean
+	public ServletContextTemplateResolver templateResolver() {
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+		templateResolver.setPrefix("/WEB-INF/templates/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode("HTML5");
+
+		return templateResolver;
+	}
+
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver());
+
+		return templateEngine;
+	}
+
+	@Override
+	public void configureDefaultServletHandling(
+			DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	@Override
+	public void configureContentNegotiation(
+			ContentNegotiationConfigurer configurer) {
+		configurer.favorParameter(false);
+		configurer.favorPathExtension(false);
+	}
 
 }
